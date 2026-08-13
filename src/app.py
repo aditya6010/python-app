557b1a49107216823581e712a771ec32810a00a8
Aditya Prasad <aditya6010@gmail.com>
2026-08-13 08:59:32 +0530

    feature1

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def get_value_details():
+    return jsonify({
+        "value": "example_value",
+        "timestamp": datetime.datetime.now().isoformat(),
+        "hostname": socket.gethostname(),
+        "message": "This is a sample response from the Flask application."
+    })
+
+@app.route('/api/v1/healthz')
+def get_healthz():
+    return jsonify({"status": "healthy"}),200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
